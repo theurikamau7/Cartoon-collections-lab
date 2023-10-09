c451b74b2f6f931e3f5492807b101f6226c6437a
IanKamau <theurikamau7@gmail.com>
2023-10-10 02:33:44 +0300

    completed cartoon collections

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i, dwarf in enumerate(dwarves, start=1):
+        print(f"{i}. {dwarf}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    result = []
+    for call in planeteer_calls:
+        result.append(call.capitalize() + "!")
+    return result
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheeses:
+            return snack
+    return None
